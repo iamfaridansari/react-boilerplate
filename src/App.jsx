2b9f057b1ca5dcb639b7d1dcfcb81044c185d660
Farid Ansari <iamfaridansari@gmail.com>
2023-05-06 10:55:13 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+//
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "./assets/css/style.css";
+import "./assets/css/responsive.css";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello world</h1>
+    </>
+  );
+};
+
+export default App;
